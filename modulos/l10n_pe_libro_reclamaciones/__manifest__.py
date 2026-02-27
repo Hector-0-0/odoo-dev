78cdfa86c850770906c8070d14dba26678b4292b
Hector <floressanchezhectordavid@gmail.com>
2026-02-27 16:01:14 -0500

    27/02/26

---
+++ modulos/l10n_pe_libro_reclamaciones/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'Libro de Reclamaciones',
+    'version': '1.0',
+    'summary': 'Módulo para gestionar el Libro de Reclamaciones según normativa INDECOPI',
+    'author': 'Hector-0-0',
+    'description': """
+Libro de Reclamaciones
+====================
+Este módulo permite a las empresas peruanas gestionar el Libro de Reclamaciones de acuerdo con la normativa establecida por el INDECOPI. Facilita el registro, seguimiento y generación de reportes relacionados con las reclamaciones de los clientes, asegurando el cumplimiento legal y mejorando la atención al cliente.
+    """,
+    'category': 'Other',
+    'depends': ['base_setup', 'base', 'mail', 'l10_pe'],
+    'installable': True,
+    'application': True,
+    'license': 'LGPL-3',
+}
